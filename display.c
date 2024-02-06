631940ad0866a2125b7bbf2cc7f5e1c68b3f1b8a
Shiva-ns <153441702+Shiva-ns@users.noreply.github.com>
2024-02-06 11:13:35 +0530

    Update display.c

--- display.c
+++ display.c
@@ -1,3 +1,7 @@
+/*The function "displayStudents" prints the information of all students in an array. 
+It checks if there are any students to display and then iterates over each student.
+prints their ID, name, email, course, and phone number. */
+
 #include "main.h"
 
 #define PAGE_SIZE 5 // Number of students to display per page
